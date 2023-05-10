04fd56331d1d9119e26897be8b16ad56363a0693
keji <63624079+kejiovo@users.noreply.github.com>
2023-05-10 16:35:44 +0800

    12

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -1,34 +1,29 @@
-async function buttonClick(e) {
-	var name = document.getElementById('name').value;
-	var vx = document.getElementById('vx').value;
-	var email = document.getElementById('email').value;
-	var message = document.getElementById('message').value;
-	
-	// 发送post
-	var url = "https://www.pushplus.plus/api/send"
-	var body = {
-		"token": "495682d8df5e4a9386658c265091b3c5",
-		"title": "网页消息",
-		"content":"姓名:" + name 
-			+ "\n微信:" + vx 
-			+ "\nemail:" + email 
-			+ "\n消息:" + message,
-		"channel":"cp",
-		    "webhook":"1433223"
-	}
-	fetch(url, { //请求的服务器地址
-		body: JSON.stringify(body), //请求的数据
-		// body:{name:"mumu",age:20}, //第二种请求数据的方法json
-		method: "POST", //请求方法
-		headers: { //请求头信息
-			// 'Content-Type':'application/x-www-form-urlencoded' //用url编码形式处理数据
-			'Content-Type': 'application/json' //第二种请求头编写方式json
-		}
-	}).then(res=>res.text()) //请求得到的数据转换为text
-       .then(res=>{
-           alert("发送成功啦，等我联系你哦")   //打印输出
-       })
-       .catch(err=>{    //错误打印
-           alert("发送失败了，网页又出bug啦")
-       })
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+window = dom.window;
+document = window.document;
+
+async function pushMessage() {
+	var name = document.getElementById('name');
+	var vx = document.getElementById('vx');
+	var email = document.getElementById('email');
+	var message = document.getElementById('message');
+	let m = `姓名:${name}\n微信:${vx}\nemail:${email}\n消息:${message}`;
+	const url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=51469656-1ebb-4acf-904d-6ca85e7224f1';
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			msgtype: 'text',
+			text: {
+				content: m
+			}
+		})
+	};
+	const response = await fetch(url, options);
+	const data = await response.json();
+	return data.errmsg === 'ok';
 }
