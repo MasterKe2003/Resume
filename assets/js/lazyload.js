b28c478796721bd8179ea3ae241a746472433b54
柯大师 <63624079+MasterKeee@users.noreply.github.com>
2023-07-20 23:32:32 +0800

    1

--- assets/js/lazyload.js
+++ assets/js/lazyload.js
@@ -1,39 +1,20 @@
-// 节流函数
-function throttle(func, delay) {
-    let timer = null;
-    return function() {
-      if (!timer) {
-        timer = setTimeout(() => {
-          func.apply(this, arguments);
-          timer = null;
-        }, delay);
-      }
-    };
-  }
-  
-  // 获取所有需要懒加载的图片和视频元素
-  const lazyElements = document.querySelectorAll('img[data-src], video[data-src]');
-  
-  // 懒加载图片和视频的函数
-  function lazyLoad() {
-    lazyElements.forEach((element) => {
-      if (element.getBoundingClientRect().top <= window.innerHeight && element.getBoundingClientRect().bottom >= 0 && getComputedStyle(element).display !== 'none') {
-        if (element.tagName === 'IMG') {
-          element.setAttribute('src', element.getAttribute('data-src'));
-        } else if (element.tagName === 'VIDEO') {
-          element.setAttribute('src', element.getAttribute('data-src'));
-          element.load();
-        }
-        element.removeAttribute('data-src');
+// 获取所有具有 data-src 属性的图片元素
+const lazyImages = document.querySelectorAll('img[data-src]');
+
+// 遍历每个图片元素
+lazyImages.forEach((img) => {
+  // 当图片进入可视区域时
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // 将 data-src 属性的值赋给 src 属性，触发图片加载
+        img.src = img.dataset.src;
+        // 停止观察该图片
+        observer.unobserve(img);
       }
     });
-  }
-  
-  // 使用节流函数优化懒加载函数
-  const throttledLazyLoad = throttle(lazyLoad, 200);
-  
-  // 页面加载完成后开始懒加载
-  window.addEventListener('load', throttledLazyLoad);
-  // 滚动页面时也触发懒加载
-  window.addEventListener('scroll', throttledLazyLoad);
-  
+  });
+
+  // 开始观察图片
+  observer.observe(img);
+});
